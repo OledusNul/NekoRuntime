39d051a6e1a0019ccb8b1b484f26ec0d973461d0
OledusNul <103176357+OledusNul@users.noreply.github.com>
2023-05-06 21:51:16 +0300

    Update LoginAuthButtonComponent.java

--- src/main/java/pro/gravit/launcher/client/gui/scenes/login/LoginAuthButtonComponent.java
+++ src/main/java/pro/gravit/launcher/client/gui/scenes/login/LoginAuthButtonComponent.java
@@ -12,31 +12,17 @@ import pro.gravit.launcher.client.gui.helper.LookupHelper;
 public class LoginAuthButtonComponent {
     private final JavaFXApplication application;
     private final Pane layout;
-    private final Pane authUnActive;
     private final Pane authActive;
-    private final SVGPath authBorder;
     private final Button button;
-
-    private final Node authsvg;
-    private boolean isDisabled;
 
     public LoginAuthButtonComponent(Pane authButton, JavaFXApplication application, EventHandler<ActionEvent> eventHandler) {
         this.application = application;
         this.layout = authButton;
-        this.authUnActive = LookupHelper.lookup(layout, "#authUnactive");
         this.authActive = LookupHelper.lookup(layout, "#authActive");
-        this.authBorder = LookupHelper.lookup(authActive, "#authBorder");
         this.button = LookupHelper.lookup(authActive, "#authButton");
-        this.authsvg = ((Pane)button.getGraphic()).getChildren().get(0);
-        this.button.setOnMouseEntered(e -> {
-            this.authBorder.setVisible(true);
-        });
-        this.button.setOnMouseExited(e -> {
-            this.authBorder.setVisible(false);
-        });
         this.button.setOnAction(eventHandler);
     }
-
+    
     public void setActive(boolean value) {
         authUnActive.setVisible(!value);
         authActive.setVisible(value);
@@ -50,8 +36,7 @@ public class LoginAuthButtonComponent {
     public void enable() {
         isDisabled = false;
         this.button.setDisable(false);
-    }
-
+    }  
     public Pane getLayout() {
         return layout;
     }
@@ -62,17 +47,5 @@ public class LoginAuthButtonComponent {
 
     public void setText(String text) {
         button.setText(text);
-    }
-
-    public void setError() {
-        authBorder.setStyle("-fx-fill: -fx-redTr");
-        authsvg.getStyleClass().removeAll("auth");
-        authsvg.getStyleClass().add("authButtonError");
-    }
-
-    public void unsetError() {
-        authBorder.setStyle("-fx-fill: -fx-greenTr");
-        authsvg.getStyleClass().removeAll("authButtonError");
-        authsvg.getStyleClass().add("auth");
     }
 }
