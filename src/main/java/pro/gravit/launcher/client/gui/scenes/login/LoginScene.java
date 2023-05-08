8fa260ba5f03efcb463ade80caa6823fd41c83da
OledusNul <103176357+OledusNul@users.noreply.github.com>
2023-05-08 16:46:04 +0300

    Update LoginScene.java

--- src/main/java/pro/gravit/launcher/client/gui/scenes/login/LoginScene.java
+++ src/main/java/pro/gravit/launcher/client/gui/scenes/login/LoginScene.java
@@ -92,7 +92,7 @@ public class LoginScene extends AbstractScene {
                 if(this.authAvailability == null && auth.list.size() > 0) {
                     changeAuthAvailability(auth.list.get(0));
                 }
-                 contextHelper.runCallback(this::loginWithGui);
+                 contextHelper.runInFxThread(this::loginWithGui);
             }), null);
             if (!application.isDebugMode()) {
                 processRequest(application.getTranslation("runtime.overlay.processing.text.launcher"), launcherRequest, (result) -> {
